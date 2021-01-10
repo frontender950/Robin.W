46e19ed210a7b3769be6daf24bfbb2651bd90cdd
frontender950 <lexabob.av@gmail.com>
2021-01-10 17:18:17 +0300

    create and fix section instagram, add media style

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
 $(document).ready(function(){
   $('.slider').slick({
     arrows: false,
-    slidesToShow:4,
+    slidesToShow:6,
     slidesToScroll:2,
     speed:200,
     infinite: false,
     touchThreshold:20,
     waitForAnimate: false,
+    responsive: [
+      {
+      breakpoint: 1440,
+      settings: {
+        slidesToShow: 4,
+      }
+    },
+      {
+      breakpoint: 992,
+      settings: 'unslick'
+    }
+  ]
+
   });
 });
