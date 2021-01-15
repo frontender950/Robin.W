6335580c2cb620580f34f661a04c6594cafc7003
frontender950 <lexabob.av@gmail.com>
2021-01-15 23:28:02 +0300

    add anchor links

--- js/main.js
+++ js/main.js
@@ -3,16 +3,15 @@ $(document).ready(function(){
   var button = $(".button");
   var buttonActive = $(".button--active");
   var menu = $(".mobile-menu");
-  var menuActive = $(".mobile-menu_active"); 
 
   button.click(function(){
     button.toggleClass("button--active");
     menu.toggleClass("mobile-menu_active");  
   buttonActive.click(function(){
     button.removeClass("button--active");
-    menu.removeClass("mobile-menu_active"); 
     });  
   });
+  
   // Слайдер slick секции firms
   $('.slider').slick({
     arrows: false,
